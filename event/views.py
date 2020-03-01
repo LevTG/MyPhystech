81472bb1ca0be753c26b3a741b29764c771f8b7b
Tanya <tanysa97@mail.ru>
2020-03-01 14:07:20 +0300

    'Update!!'

--- event/views.py
+++ event/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from django.core import serializers
 
-
+@api_view(['GET'])
 def home(request):
     return HttpResponse('Hello world')
 
